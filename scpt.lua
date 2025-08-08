65a670d38c3c513820954d0a10f1fed4855e4423
Ysunza <stewardkimysunza@gmail.com>
2025-08-09 00:30:12 +0800

    Update scpt.lua

--- scpt.lua
+++ scpt.lua
@@ -1,38 +1,58 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
+-- Create Main Window
 local Window = Rayfield:CreateWindow({
-   Name = "Rayfield Example Window",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Rayfield Interface Suite",
-   LoadingSubtitle = "by Sirius",
-   ShowText = "Rayfield", -- for mobile users to unhide rayfield, change if you'd like
-   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
-
-   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
-
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+   Name = "NunHub",
+   Icon = 0,
+   LoadingTitle = "Loading NunHub",
+   LoadingSubtitle = "powered by Rayfield",
+   ShowText = "NunHub",
+   Theme = "Default",
+   ToggleUIKeybind = "K",
 
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
+      FolderName = "NunHubConfig",
+      FileName = "NunHubSettings"
    },
 
    Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+      Enabled = false,
+      Invite = "noinvitelink",
+      RememberJoins = true
    },
 
-   KeySystem = false, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Untitled",
-      Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
+   KeySystem = false
+})
+
+-- HOME TAB
+local HomeTab = Window:CreateTab("Home", 4483362458) -- Icon ID from Roblox
+
+HomeTab:CreateButton({
+    Name = "Join Discord",
+    Callback = function()
+        setclipboard("https://discord.gg/YourInviteHere") -- Copies Discord link to clipboard
+        Rayfield:Notify({
+            Title = "Discord Link Copied",
+            Content = "Invite link has been copied to your clipboard!",
+            Duration = 5
+        })
+    end
+})
+
+-- SETTINGS TAB
+local SettingsTab = Window:CreateTab("Settings", 4483362458)
+
+SettingsTab:CreateDropdown({
+    Name = "Select Theme",
+    Options = {"Default", "Light", "Dark", "Ocean", "Tokyo Night"},
+    CurrentOption = "Default",
+    Callback = function(option)
+        Rayfield:SetTheme(option)
+        Rayfield:Notify({
+            Title = "Theme Changed",
+            Content = "Theme set to " .. option,
+            Duration = 3
+        })
+    end
 })
